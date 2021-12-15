9439700430ce5f7b1adba79033c49ac59b65b632
Samuel Lee <lees@satalyst.com>
2021-12-15 11:33:20 +0800

    testrigger

--- SampleConsoleNet5/SampleConsoleNet5/Program.cs
+++ SampleConsoleNet5/SampleConsoleNet5/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleNet5
     {
         static void Main(string[] args)
         {
-            // testfaassdaasdsfgdfdfdsererdfsdetssfsddffasd
+            // testfaassdaasdsfgdfdfdsererdfsdetsasdsfsddffasd
             Console.WriteLine("Hello World!");
         }
     }
