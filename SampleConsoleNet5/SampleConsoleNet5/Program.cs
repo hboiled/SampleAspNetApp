7a757bf6273768c35f92411e2d20ffe5af16500f
Samuel Lee <lees@satalyst.com>
2021-12-15 15:10:14 +0800

    test trigger 2

--- SampleConsoleNet5/SampleConsoleNet5/Program.cs
+++ SampleConsoleNet5/SampleConsoleNet5/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleNet5
     {
         static void Main(string[] args)
         {
-            // testfaassdaasdsfgdfdfdsererdfsdetssdfsdasdsfsddsdfsdffasdsdfsdsfd
+            // testfaassdaasdsfgdfdfdsererdfsdetssdfsdasdsfsddasdfsdffasdsdfsdsfd
             Console.WriteLine("Hello World!");
         }
     }
