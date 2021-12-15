a54f24d7dd5c1854857d040066469083836fe76f
Samuel Lee <lees@satalyst.com>
2021-12-15 10:37:45 +0800

    test 6

--- SampleConsoleNet5/SampleConsoleNet5/Program.cs
+++ SampleConsoleNet5/SampleConsoleNet5/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleNet5
     {
         static void Main(string[] args)
         {
-            // testfaa
+            // testfaas
             Console.WriteLine("Hello World!");
         }
     }
